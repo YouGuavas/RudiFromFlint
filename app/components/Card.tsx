5cd05a2316dceefb9de167774c73ed4d5c1bbb64
YouGuavas <patrickyambrick@gmail.com>
2025-06-05 18:38:46 -0400

    Add cars to carousel

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,28 +1,42 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Card(image: any) {
+type propsType = {
+	model: string;
+	category: string;
+	imageSrc: string;
+
+	year: number;
+	make: string;
+	mileage: number;
+};
+
+export default function Card(props: propsType) {
 	return (
-		<>
-			<h1 className={`style-1 uppercase thin`}>Blue Car One.</h1>
-			<Image src={image.src} height={150} width={300} alt="Car Icon" />
+		<div className={`flex column center`}>
+			<h1 className={`style-1 uppercase thin`}>
+				{`${props.make} ${props.model}`}.
+			</h1>
+			<div className={`image-container`}>
+				<Image src={props.imageSrc} fill alt="Car Icon" />
+			</div>
 			<p className={`paragraph width-full block left-align`}>
-				Blue Car One is a dignified ride.
+				{`${props.make} ${props.model} is a ${props.category}.`}
 			</p>
 			<ul className={`flex column gap-medium left-align width-full no-deco`}>
 				<li>
-					<strong>Make:</strong> Blue
+					<strong>Make:</strong> {props.make}
 				</li>
 				<li>
-					<strong>Model:</strong> Car
+					<strong>Model:</strong> {props.model}
 				</li>
 				<li>
-					<strong>Mileage:</strong> One
+					<strong>Mileage:</strong> {props.mileage}
 				</li>
 				<li>
-					<strong>Year:</strong> One
+					<strong>Year:</strong> {props.year}
 				</li>
 			</ul>
-		</>
+		</div>
 	);
 }
--- app/components/car-ousel.tsx
+++ app/components/car-ousel.tsx
@@ -1,12 +1,47 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import blue_car from '../../public/logos/blue_car.svg';
+import red_car from '../../public/logos/red_car.svg';
+import green_car from '../../public/logos/green_car.svg';
+import black_car from '../../public/logos/black_car.svg';
+
 import Card from './Card';
 
 export default function Carousel() {
 	return (
-		<>
-			<Card image={blue_car} />
-		</>
+		<div className={`flex row`}>
+			<Card
+				imageSrc={blue_car.src}
+				model={`Car`}
+				make={`Blue`}
+				category={`dignified ride`}
+				year={1994}
+				mileage={125000}
+			/>
+			<Card
+				imageSrc={red_car.src}
+				model={`Car`}
+				make={`Red`}
+				category={`dignified ride`}
+				year={1998}
+				mileage={160000}
+			/>
+			<Card
+				imageSrc={green_car.src}
+				model={`Car`}
+				make={`Green`}
+				category={`dignified ride`}
+				year={2006}
+				mileage={150000}
+			/>
+			<Card
+				imageSrc={black_car.src}
+				model={`Car`}
+				make={`Black`}
+				category={`dignified ride`}
+				year={2002}
+				mileage={170000}
+			/>
+		</div>
 	);
 }
